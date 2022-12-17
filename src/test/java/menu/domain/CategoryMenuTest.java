2d8cae69b5c0dd83351df3cc335c02dd4c1d796a
Youngho <cyh6099@gmail.com>
2022-12-17 14:55:22 +0900

    test : 카테코리에 맞는 메뉴 리스트 반환 여부 테스트

--- src/test/java/menu/domain/CategoryMenuTest.java
+++ src/test/java/menu/domain/CategoryMenuTest.java
@@ -3,6 +3,8 @@ package menu.domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 class CategoryMenuTest {
 
     @Test
@@ -15,5 +17,20 @@ class CategoryMenuTest {
     void 숫자에_맞는_카테고리_반환_테스트() {
         org.assertj.core.api.Assertions.assertThat(CategoryMenu.getCategory(1))
                 .isEqualTo(CategoryMenu.JAPAN_FOOD);
+    }
+
+    @Test
+    void 카테고리에_맞는_메뉴_리스트_반환_테스트() {
+        org.assertj.core.api.Assertions.assertThat(CategoryMenu.getMenus(CategoryMenu.KOREA_FOOD))
+                .containsExactly("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음");
+    }
+
+    @Test
+    void 원본_메뉴_리스트는_불변() {
+        List<String> baseMenus = CategoryMenu.getMenus(CategoryMenu.KOREA_FOOD);
+        baseMenus.set(0, "바나나");
+        List<String> newMenus = CategoryMenu.getMenus(CategoryMenu.KOREA_FOOD);
+
+        org.assertj.core.api.Assertions.assertThat(newMenus).doesNotContain("바나나");
     }
 }
