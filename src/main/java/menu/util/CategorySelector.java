d73458b478b8c9d564ff4707345504a69077edea
Youngho <cyh6099@gmail.com>
2022-12-17 17:16:11 +0900

    refactor : 상수화

--- src/main/java/menu/util/CategorySelector.java
+++ src/main/java/menu/util/CategorySelector.java
@@ -4,11 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 import menu.domain.CategoryMenu;
 
 public class CategorySelector {
+    private final int MIN_CATEGORY_NUMBER = 1;
+    private final int MAX_CATEGORY_NUMBER = 5;
+
+    private final int PICK_MENU_NUMBER = 0;
+
     public CategoryMenu pickCategory() {
-        return CategoryMenu.getCategory(Randoms.pickNumberInRange(1, 5));
+        return CategoryMenu.getCategory(Randoms.pickNumberInRange(MIN_CATEGORY_NUMBER, MAX_CATEGORY_NUMBER));
     }
 
     public String pickMenu(CategoryMenu categoryMenu) {
-        return Randoms.shuffle(CategoryMenu.getMenus(categoryMenu)).get(0);
+        return Randoms.shuffle(CategoryMenu.getMenus(categoryMenu)).get(PICK_MENU_NUMBER);
     }
 }
