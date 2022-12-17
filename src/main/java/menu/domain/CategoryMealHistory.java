691b824b2f9d1cbd6e7d2eb36336eaee552ea025
Youngho <cyh6099@gmail.com>
2022-12-17 16:31:07 +0900

    feat : 요일 별 추천된 카테코리 저장하는 기능 구현

--- src/main/java/menu/domain/CategoryMealHistory.java
+++ src/main/java/menu/domain/CategoryMealHistory.java
@@ -1,9 +1,12 @@
 package menu.domain;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
+import java.util.List;
 
 public class CategoryMealHistory {
     private final EnumMap<CategoryMenu, Integer> categoryHistory = new EnumMap<>(CategoryMenu.class);
+    private final List<CategoryMenu> dayRecommendedCategoryInfo = new ArrayList<>();
 
     public boolean isPossibleCategory(CategoryMenu category) {
         Integer mealCount = categoryHistory.getOrDefault(category, 0);
@@ -12,5 +15,13 @@ public class CategoryMealHistory {
 
     public void mealCategory(CategoryMenu categoryMenu) {
         categoryHistory.put(categoryMenu, categoryHistory.getOrDefault(categoryMenu, 0) + 1);
+    }
+
+    public void saveCategoryInfoPerDay(CategoryMenu category) {
+        dayRecommendedCategoryInfo.add(category);
+    }
+
+    public List<CategoryMenu> getCategoryRecommendedInfo() {
+        return new ArrayList<>(dayRecommendedCategoryInfo);
     }
 }
