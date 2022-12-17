ed8525bedb5e70a1b724d3bd35dea50974077e66
Youngho <cyh6099@gmail.com>
2022-12-17 15:52:17 +0900

    fix : 에러 메시지 수정

--- src/main/java/menu/domain/CoachInfo.java
+++ src/main/java/menu/domain/CoachInfo.java
@@ -43,13 +43,13 @@ public class CoachInfo {
 
     private void validateNameLength(String coachName) {
         if (coachName.length() < MIN_NAME_LENGTH || coachName.length() > MAX_NAME_LENGTH) {
-            throw new IllegalArgumentException("이름의 길이가 허용 범위 밖입니다.");
+            throw new IllegalArgumentException(" 이름의 길이가 허용 범위 밖입니다.");
         }
     }
 
     private void validateCoachNumber(List<String> coachNames) {
         if (coachNames.size() < MIN_COACH_NUMBER || coachNames.size() > MAX_COACH_NUMBER) {
-            throw new IllegalArgumentException("코치는 2~5명 사이만 가능합니다.");
+            throw new IllegalArgumentException(" 코치는 2~5명 사이만 가능합니다.");
         }
     }
 }
