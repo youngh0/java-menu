0a88ec0e40cebc73b5154f4519a8be5e9269f0e7
Youngho <cyh6099@gmail.com>
2022-12-17 15:20:19 +0900

    test : 코치 별 새로운 메뉴에 대해 못 먹는 메뉴인지 판단하는 기능 테스트

--- src/test/java/menu/domain/CoachInfoTest.java
+++ src/test/java/menu/domain/CoachInfoTest.java
@@ -3,6 +3,7 @@ package menu.domain;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
@@ -22,6 +23,18 @@ class CoachInfoTest {
     void 코치_인원_유효성_테스트(List<String> coachInfo) {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new CoachInfo(coachInfo));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"토미,우동,true","포코,스시,false","제임스,우동,false"})
+    void 코치_못_먹는_음식인지_판단_테스트(String name, String menu, boolean result) {
+        CoachInfo coachInfo = new CoachInfo(List.of("토미","제임스","포코"));
+        coachInfo.initForbiddenFood("토미", List.of("우동", "스시"));
+        coachInfo.initForbiddenFood("제임스", List.of("뇨끼", "월남쌈"));
+        coachInfo.initForbiddenFood("포코", List.of());
+
+        org.assertj.core.api.Assertions.assertThat(coachInfo.isForbiddenMenu(name, menu))
+                .isEqualTo(result);
     }
 
     static Stream<Arguments> generateWrongNameLengthData() {
