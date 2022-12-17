108a15e9fbaf8369eb0c6165dc7d2143eab99173
Youngho <cyh6099@gmail.com>
2022-12-17 14:05:49 +0900

    feat : 못 먹는 음식 입력 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 import menu.domain.CoachInfo;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class InputView {
     private final String INPUT_COACH_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
@@ -11,5 +13,13 @@ public class InputView {
     public CoachInfo readCoachNames() {
         System.out.println(INPUT_COACH_NAME_MESSAGE);
         return new CoachInfo(Arrays.asList(Console.readLine().split(",")));
+    }
+
+    public List<String> readForbiddenMenu() {
+        String menus = Console.readLine();
+        if (menus.equals("")) {
+            return Collections.emptyList();
+        }
+        return Arrays.asList(menus.split(","));
     }
 }
