a7009b6bb51a9f0e87f6729482e4871faf4b14b5
Youngho <cyh6099@gmail.com>
2022-12-17 14:05:33 +0900

    feat : 게임 시작 출력문 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,4 +1,8 @@
 package menu.view;
 
 public class OutputView {
+    private final String START_MESSAGE = "점심 메뉴 추천을 시작합니다.";
+    public void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
 }
