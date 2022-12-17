76be6d9144f2eab148ccdeb8dc3c90cc8d932ba1
Youngho <cyh6099@gmail.com>
2022-12-17 14:00:43 +0900

    feat : 코치 이름 입력받는 inputView구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,15 @@
 package menu.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import menu.domain.CoachInfo;
+
+import java.util.Arrays;
+
 public class InputView {
+    private final String INPUT_COACH_NAME_MESSAGE = "코치의 이름을 입력해 주세요. (, 로 구분)";
+
+    public CoachInfo readCoachNames() {
+        System.out.println(INPUT_COACH_NAME_MESSAGE);
+        return new CoachInfo(Arrays.asList(Console.readLine().split(",")));
+    }
 }
