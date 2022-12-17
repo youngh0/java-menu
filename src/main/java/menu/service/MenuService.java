48a8ad3690c8322c279d2019152d7d17d946fe71
Youngho <cyh6099@gmail.com>
2022-12-17 15:53:28 +0900

    feat : 일주일 간 조건에 맞게 메뉴 추천하는 단위 기능 통합한 메서드 구현

--- src/main/java/menu/service/MenuService.java
+++ src/main/java/menu/service/MenuService.java
@@ -2,14 +2,24 @@ package menu.service;
 
 import menu.domain.CategoryMealHistory;
 import menu.domain.CategoryMenu;
+import menu.domain.CoachInfo;
+import menu.domain.CoachMealHistory;
 import menu.util.CategorySelector;
 
 public class MenuService {
     private final CategoryMealHistory categoryMealHistory = new CategoryMealHistory();
+    private final CoachMealHistory coachMealHistory = new CoachMealHistory();
     private final CategorySelector categorySelector = new CategorySelector();
 
-    public void recommendMenu() {
-        CategoryMenu category = findPossibleCategory();
+    public void recommendMenu(CoachInfo coachInfo) {
+        int oneWeek = 5;
+        while (oneWeek-- > 0) {
+            CategoryMenu category = pickPossibleCategory();
+            for (String coachName : coachInfo.getCoachNames()) {
+                String menu = pickPossibleMenu(category, coachName, coachInfo);
+                coachMealHistory.mealMenu(coachName, menu);
+            }
+        }
     }
 
     private CategoryMenu findPossibleCategory() {
@@ -18,5 +28,21 @@ public class MenuService {
             return category;
         }
         return findPossibleCategory();
+    }
+
+    private CategoryMenu pickPossibleCategory() {
+        CategoryMenu category = findPossibleCategory();
+        if (categoryMealHistory.isPossibleCategory(category)) {
+            return category;
+        }
+        return pickPossibleCategory();
+    }
+
+    private String pickPossibleMenu(CategoryMenu category, String coachName, CoachInfo coachInfo) {
+        String menu = categorySelector.pickMenu(category);
+        if (coachMealHistory.isPossibleMenu(coachName, menu) && !coachInfo.isForbiddenMenu(coachName,menu)) {
+            return menu;
+        }
+        return pickPossibleMenu(category, coachName, coachInfo);
     }
 }
