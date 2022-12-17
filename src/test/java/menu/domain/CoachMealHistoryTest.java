1ba22f8b2bf255ad0de832c1f7e3b58d61c62b03
Youngho <cyh6099@gmail.com>
2022-12-17 15:12:11 +0900

    test : 코치 별 메뉴 식사 여부에 따라 현재 메뉴 식사 가능한지 판단 테스트

---
+++ src/test/java/menu/domain/CoachMealHistoryTest.java
@@ -0,0 +1,18 @@
+package menu.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoachMealHistoryTest {
+    CoachMealHistory coachMealHistory = new CoachMealHistory();
+    @ParameterizedTest
+    @CsvSource(value = {"영호,밥,false","영호,국,true"})
+    void isPossibleMenu(String name, String menu, boolean result) {
+        coachMealHistory.mealMenu("영호","밥");
+        Assertions.assertThat(coachMealHistory.isPossibleMenu(name, menu)).isEqualTo(result);
+    }
+}
