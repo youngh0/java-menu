96949cca5a0784284fc5b4ec51e293156d9da074
Youngho <cyh6099@gmail.com>
2022-12-17 13:51:29 +0900

    feat : 카테고리 별 메뉴 이름, 메뉴 존재 여부 확인하는 기능 구현

---
+++ src/main/java/menu/domain/CategoryMenu.java
@@ -0,0 +1,34 @@
+package menu.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum CategoryMenu {
+    JAPAN_FOOD(List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스","라멘","오코노미야끼"), "일식",1),
+    KOREA_FOOD(List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기","떡볶이","제육볶음"), "한식",2),
+    CHINA_FOOD(List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육","토마토 달걀볶음","고추잡채"), "중식",3),
+    ASIA_FOOD(List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미","월남쌈","분짜"), "아시안",4),
+    WEST_FOOD(List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티","피자","파니니"), "양식",5);
+
+    private final List<String> menus;
+    private final String categoryName;
+    private final int categoryNumber;
+
+    CategoryMenu(List<String> menus, String categoryName, int categoryNumber) {
+        this.menus = menus;
+        this.categoryName = categoryName;
+        this.categoryNumber = categoryNumber;
+    }
+
+    public static void validateMenuName(String menu) {
+        Arrays.stream(CategoryMenu.values())
+                .filter(category -> category.menus.contains(menu))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메뉴입니다."));
+    }
+}
+//일식: 규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼
+//한식: 김밥, 김치찌개, 쌈밥, 된장찌개, 비빔밥, 칼국수, 불고기, 떡볶이, 제육볶음
+//중식: 깐풍기, 볶음면, 동파육, 짜장면, 짬뽕, 마파두부, 탕수육, 토마토 달걀볶음, 고추잡채
+//아시안: 팟타이, 카오 팟, 나시고렝, 파인애플 볶음밥, 쌀국수, 똠얌꿍, 반미, 월남쌈, 분짜
+//양식: 라자냐, 그라탱, 뇨끼, 끼슈, 프렌치 토스트, 바게트, 스파게티, 피자, 파니니
